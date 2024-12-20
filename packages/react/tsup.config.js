1246fc70c852d25637b1dc33b6ed841b6df3e87b
rcharmeyer <rcharmeyer@hotmail.com>
2024-12-19 16:35:13 -0800

    Refactor @reconjs/react package.json and tsup.config.js for improved module exports and build configurations
    - Reintroduced "type": "module" in package.json for better module handling.
    - Updated exports in package.json to differentiate between ESM and CommonJS formats.
    - Added legacyConfig function in tsup.config.js to support legacy build configurations alongside modern ones.
    - Enhanced build options with source maps and TypeScript definitions for legacy builds.

--- packages/react/tsup.config.js
+++ packages/react/tsup.config.js
@@ -25,6 +25,28 @@ function modernConfig(opts) {
   }
 }
 
+/**
+ * @param {Object} opts - Options for building configurations.
+ * @param {string[]} opts.entry - The entry array.
+ * @returns {import('tsup').Options}
+ */
+export function legacyConfig(opts) {
+  return {
+    entry: opts.entry,
+    format: ['cjs', 'esm'],
+    target: ['es2020', 'node16'],
+    outDir: 'build/legacy',
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    esbuildPlugins: [
+      // @ts-expect-error
+      esbuildPluginFilePathExtensions({ esmExtension: 'js' })
+    ],
+  }
+}
+
 export default defineConfig([
   modernConfig({ entry: ['src/*.ts', 'src/*.tsx'] }),
+  legacyConfig({ entry: ['src/*.ts', 'src/*.tsx'] })
 ])
